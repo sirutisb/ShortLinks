9dad0c39e6cc7cace543c54f3e273b77e04146b2
sirutisb <87762791+sirutisb@users.noreply.github.com>
2025-04-01 23:24:24 +0100

    Update LinkController.java

--- src/main/java/com/example/shortlinks/LinkController.java
+++ src/main/java/com/example/shortlinks/LinkController.java
@@ -6,7 +6,6 @@ import com.example.shortlinks.dto.LinkStatsResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 @RequestMapping("/api")
